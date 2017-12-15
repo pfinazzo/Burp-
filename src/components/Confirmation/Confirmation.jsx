49644d75a12ed282d994b423e459b5549d3305d1
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-12-14 23:03:40 -0800

    add name to confirmation page

--- src/components/Confirmation/Confirmation.jsx
+++ src/components/Confirmation/Confirmation.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Confirmation = () => {
+const Confirmation = (props) => {
   return (
     <div className="center">
-      <h1>Congratulations! Beer is on the way!</h1>
+      <h1>Congratulations {props.user.name}! Beer is on the way!</h1>
       </div>
   )
 }
